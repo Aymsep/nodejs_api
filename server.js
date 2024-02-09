d7c1b4538a5768cec7d85e6f962785e90666c10a
Aymsep <71574687+Aymsep@users.noreply.github.com>
2024-02-09 16:13:41 +0000

    added up

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
 const http = require('http');
 const url = require('url');
+require("dotenv").config()
 
-require('dotenv').config()
 
 const routes = {
-    "/home":function(req, res){
-        return res.end('welcom to homepage')
-    },
-    "/login":function(req, res){
-        return res.end('user logged succeffuly')
-    },
-    '/about':function(req, res){
-        return res.end('user data')
+    '/admin-panel':function(req,res){
+        return res.end('Welcome to page home')
     }
 }
 
-const Port = process.env.PORT || 5000
-
-const server = http.createServer((req,res)=>{
-
-    // let {query:{name,email,role}} = url.parse(req.url,true)
-    // if(role === 'admin'){
-    //     res.writeHead(200)
-    //     res.write(`hello ${name} with email ${email} you allowed to be here`)
-    //     return res.end()
-    // }
-    // return res.end('u are not allowed to be here')
-    // res.write({
-    //     name,
-    //     email,
-    //     role
+const server = http.createServer((req, res) => {
+    console.log('user entered')
+    // req.on('data',(data)=>{
+    //     console.log(JSON.parse(data).number)
     // })
-    // res.end()
-//   if(req.url in routes){
-//     return routes[req.url](req, res);
-//   }
-//   return res.end('route not found')
-
+    // console.log(url.parse(req.url))
+    // const {pathname,query:{name,age,role}} = url.parse(req.url,true)
+    // console.log(name,age,role)
+    // if(pathname in routes){
+    //     if(role === 'admin'){
+    //         return res.end(`Welcome to admin panel M.${name}`)
+    //     }
+    //     return res.end('you are not allowed to access this page')
+    // }
+    // return res.end('404 not found')
+//    if(req.url in routes) {
+//     return routes[req.url](req,res)
+//    }
+//    return res.end('page not found')
+    
 })
 
+const port = process.env.PORT || 3500
 
-
-server.listen(process.env.PORT || 5000,()=>{
-    console.log('listening on port '+Port)
+server.listen(port,()=>{
+    console.log('listening on port '+port)
 })
+
--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-function add(){}
-function sum(){}
-function minus(){}
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+console.log('entred userdd')
+}).listen(3500,()=>{
+    console.log('listening on port 3500')
+})
+
+
